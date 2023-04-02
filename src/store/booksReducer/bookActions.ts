6c68ac2da8e0d9cf03378e860781a425df9e9397
Andrey <spacecmetanka@gmail.com>
2023-04-02 15:24:28 +0300

    refactor: books store

--- src/store/booksReducer/bookActions.ts
+++ src/store/booksReducer/bookActions.ts
@@ -1,16 +1,33 @@
-import { IBook } from 'service/types';
-import { BOOKS_ACTIONS } from './types';
+import { Dispatch } from 'react';
+import { basicURL, maxResults } from 'service/consts';
+import { RootState } from 'store';
+import { BOOKS_ACTIONS, IBooksActions } from './types';
 
 export const bookActions = {
-  setBooks: (payload: { books: IBook[]; total: number }) => ({
-    type: BOOKS_ACTIONS.SET_BOOKS,
-    payload,
-  }),
-  updateBooks: (payload: IBook[]) => ({
-    type: BOOKS_ACTIONS.UPDATE_BOOKS,
-    payload,
-  }),
   resetBooks: () => ({
     type: BOOKS_ACTIONS.RESET_BOOKS,
   }),
+
+  loadBooks: () => {
+    return async (dispatch: Dispatch<IBooksActions>, getState: () => RootState) => {
+      try {
+        const { searchQuery, sortType, categories, startIndex } = getState().filterReducer;
+        const URL = `${basicURL}${searchQuery}+subject:${categories}&maxResults=${maxResults}&startIndex=${startIndex}&orderBy=${sortType}&${process.env.API_KEY}`;
+        const { items, totalItems } = await fetch(URL).then((res) => res.json());
+
+        dispatch({
+          type: BOOKS_ACTIONS.SET_BOOKS,
+          payload: { books: items, total: totalItems },
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  },
+
+  getBookById: (id: string) => {
+    return (dispatch: Dispatch<IBooksActions>) => {
+      console.log('gebookbyid');
+    };
+  },
 };
--- src/store/booksReducer/types.ts
+++ src/store/booksReducer/types.ts
@@ -2,7 +2,6 @@ import { IBook } from 'service/types';
 
 export enum BOOKS_ACTIONS {
   SET_BOOKS = 'SET_BOOKS',
-  UPDATE_BOOKS = 'UPDATE_BOOKS',
   RESET_BOOKS = 'RESET_BOOKS',
 }
 
@@ -14,14 +13,8 @@ interface ISetBookAction {
   };
 }
 
-interface IUpdateBookActin {
-  type: BOOKS_ACTIONS.UPDATE_BOOKS;
-  payload: IBook[];
-}
-
 interface IResetBookAction {
   type: BOOKS_ACTIONS.RESET_BOOKS;
-  payload: IBook[];
 }
 
-export type IBooksActions = ISetBookAction | IUpdateBookActin | IResetBookAction;
+export type IBooksActions = ISetBookAction | IResetBookAction;
